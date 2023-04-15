be579fa62a23e46760ace58b8cc3e11a567cd285
Botang <83536674+Botang-l@users.noreply.github.com>
2023-04-15 18:51:51 +0800

    add local server

--- server.py
+++ server.py
@@ -2,4 +2,4 @@ from flaskr import app, db
 
 if __name__ == '__main__':
     
-    app.run(host='0.0.0.0', port=54088, debug = True)
+    app.run(host='0.0.0.0', port=54088, ssl_context='adhoc', debug = True)
